4a26e4a3717ae578a328fdf40c47c8e30bb5594a
MysticalMHM <95140666+MysticalMHM@users.noreply.github.com>
2024-03-03 11:58:04 -0800

    add todo in calc.c

--- src/calc.c
+++ src/calc.c
@@ -8,7 +8,8 @@ int main(int argc, char* argv[]){
 	sscanf(argv[1], "%lf", &n1);
 	sscanf(argv[2], "%lf", &n2);
 	sscanf(argv[3], "%c", &op);
-	/*if (){
+	/* TODO: HELP MESSAGE:
+ 	if (){
 		printf("usage:\n calc(ulate) [-h] n1 n2 op\n n1: double\n n2: double\n op: any char in array ['+','-','*','/']\n returns double\n\r");
 		return 0;
 	}*/
